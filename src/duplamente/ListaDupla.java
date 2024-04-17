261b736507e1d9b8a231b7a921b7bece594e0e03
Rodrigo Araujo <rasch.dev@gmail.com>
2024-04-17 15:01:59 -0300

    Questões 9, 10 e 11

--- src/duplamente/ListaDupla.java
+++ src/duplamente/ListaDupla.java
@@ -113,4 +113,122 @@ public class ListaDupla {
 		
 		return msg;
 	}
+	
+	/*
+	 * MÉTODOS A IMPLEMENTAR
+	 * System.out.println("Informar número do procedimento pretendido:");
+		System.out.println("1. Inserir valor");
+		System.out.println("2. Pesquisar valor");
+		System.out.println("3. Remover valor");
+		System.out.println("4. Concatenar listas");                                     questão 6
+		System.out.println("5. Pegar primeira metade da lista");                        questão 7
+		System.out.println("6. Ver maior valor da lista");                              questão 8
+		System.out.println("7. Média dos valores da lista");                            questão 9
+		System.out.println("8. Retirar o número 0 da lista");                           questão 10
+		System.out.println("9. Retirar números repetidos da lista");                    questão 11 
+		System.out.println("10. Mostrar jogadores acima de 2 metros de altura");        questão 12
+		System.out.println("11. Criar lista com produtos enlatados");                   questão 13
+		System.out.println("12. Verificar palíndromo");                                 questão 14
+		System.out.println("13. Ordenar e/ou aumentar lista ordenada");                 questão 15
+		System.out.println("14. Sair");
+	 */
+	
+	//  LISTA 04 - QUESTÃO 6 - Implemente método que concatene as listas L1 (this) e L2 (recebida por parâmetro)
+	public ListaDupla concatenarListas(ListaDupla L2) {
+		this.qtdNos += L2.qtdNos;
+		this.ultimo.setProximo(L2.primeiro);
+		L2.primeiro.setAnterior(this.ultimo);
+		this.ultimo = L2.ultimo;
+		L2.primeiro = null;
+		L2.ultimo = null;
+		L2.qtdNos = 0;
+		return this;
+	}
+	
+	//  LISTA 04 - QUESTÃO 7 - Retornar primeira metade da lista L1
+	public ListaDupla primeiraMetade() {
+		int qtdRetornada = this.qtdNos/2;
+		ListaDupla listaRetornada = new ListaDupla();
+		
+		if (this.qtdNos == 1) {
+			listaRetornada = this;
+		}
+		else {
+			int count = 0;
+			NoDupla atual = this.primeiro;
+			
+			while (count < qtdRetornada) {
+				listaRetornada.inserirUltimo(new Item(atual.getInfo().getChave()));
+				atual = atual.getProximo();
+			}
+		}
+		
+		return listaRetornada;
+	}
+	
+	//  LISTA 04 - QUESTÃO 8 - Mostrar maior valor da lista
+	public int maiorValor() {
+		int maiorValor = 0;
+		NoDupla atual = this.primeiro;
+		
+		if (this.listaVazia()) {
+			return -1;
+		}
+		else {
+			while(atual != null) {
+				if (atual.getInfo().getChave() > maiorValor) {
+					maiorValor = atual.getInfo().getChave();
+				}
+				atual = atual.getProximo();
+			}
+		}
+		
+		return maiorValor;
+	}
+	
+	//  LISTA 04 - QUESTÃO 9 - Calcular média aritmética
+	public double mediaAritmetica() {
+		double media = 0;
+		int total = 0;
+		NoDupla atual = this.primeiro;
+		
+		if (this.listaVazia()) {
+			return -1.0;
+		}
+		else {
+			while (atual != null) {
+				total += atual.getInfo().getChave();
+				atual = atual.getProximo();
+			}
+			
+			media = total/this.qtdNos;
+		}
+		
+		return media;
+	}
+	
+	//  LISTA 04 - QUESTÃO 10 - Retirar os zeros da lista
+	public String retiraZero() {
+		int count = 0;
+		NoDupla atual = this.primeiro;
+		
+		while (atual != null) {
+			if (atual.getInfo().getChave() == 0) {
+				count++;
+			}
+		}
+		
+		if (count == 0) {
+			return "Não existe 0 na lista.";
+		}
+		else {
+			for (int i = 0; i < count; i++) {
+				this.removerNo(0);
+			}
+			
+			return "Todos os zeros foram removidos.";
+		}
+	}
+	
+	//  LISTA 04 - QUESTÃO 11 - Retirar valores repetidos
 }
--- src/duplamente/MenuPrincipalDupla.java
+++ src/duplamente/MenuPrincipalDupla.java
@@ -21,16 +21,25 @@ public class MenuPrincipalDupla {
 	public static boolean Menu(ListaDupla L1) {
 		int procedimento = 0, chave = 0;
 		
-		
 		System.out.println("Informar número do procedimento pretendido:");
 		System.out.println("1. Inserir valor");
 		System.out.println("2. Pesquisar valor");
 		System.out.println("3. Remover valor");
-		System.out.println("4. Sair");
+		System.out.println("4. Concatenar listas");
+		System.out.println("5. Pegar primeira metade da lista");
+		System.out.println("6. Ver maior valor da lista");
+		System.out.println("7. Média dos valores da lista");
+		System.out.println("8. Retirar o número 0 da lista");
+		System.out.println("9. Retirar números repetidos da lista");
+		System.out.println("10. Mostrar jogadores acima de 2 metros de altura");
+		System.out.println("11. Criar lista com produtos enlatados");
+		System.out.println("12. Verificar palíndromo");
+		System.out.println("13. Ordenar e/ou aumentar lista ordenada");
+		System.out.println("14. Sair");
 		
 		procedimento = scan.nextInt();
 		
-		if (procedimento == 4) {
+		if (procedimento == 14) {
 			System.out.println("O programa será encerrado.");
 			return false;
 		}
@@ -64,6 +73,36 @@ public class MenuPrincipalDupla {
 				else {
 					System.out.println("O valor " + chave + " foi removido da lista.");
 				}
+				return true;
+			case 4:
+				
+				return true;
+			case 5:
+							
+				return true;
+			case 6:
+				
+				return true;
+			case 7:
+				
+				return true;
+			case 8:
+				
+				return true;
+			case 9:
+				
+				return true;
+			case 10:
+				
+				return true;
+			case 11:
+				
+				return true;
+			case 12:
+				
+				return true;
+			case 13:
+				
 				return true;
 			default:
 				System.out.println("Opção inválida, o programa será encerrado.");
