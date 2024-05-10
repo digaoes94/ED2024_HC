d2cb808e938f567c70a13ac406923e58b7993331
Rodrigo Araujo <rasch.dev@gmail.com>
2024-05-10 03:31:07 -0300

    Questões 1, 2 e 3

--- src/filaCircular/FilaCircular.java
+++ src/filaCircular/FilaCircular.java
@@ -1,6 +1,7 @@
 package filaCircular;
 
 import dados.Item;
+import pilha.PilhaContig;
 
 public class FilaCircular {
 	private Item[] info;
@@ -49,17 +50,17 @@ public class FilaCircular {
 		}
 	}
 	
-	public boolean desenfileirar() {
+	public Item desenfileirar() {
 		Item item;
 		
 		if (this.eVazia()) {
-			return false;
+			return null;
 		}
 		else {
 			item = this.info[this.frente];
 			this.frente = (++this.frente % this.info.length);
 			this.tamanho--;
-			return true;
+			return item;
 		}
 	}
 	
@@ -74,4 +75,65 @@ public class FilaCircular {
 		
 		return msg;
 	}
+	
+	/*
+	 * System.out.println("Selecione o procedimento a executar:");
+		System.out.println("1. Enfileirar valor");
+		System.out.println("2. Desenfileirar valor");
+		System.out.println("3. Imprimir fila");
+		System.out.println("4. Retirar valores negativos da fila");
+		System.out.println("5. Somar os valores da fila");
+		System.out.println("6. Transferir valores de uma pilha para uma fila");
+		System.out.println("7. Imprimir documento em ordem decrescente");
+		System.out.println("8. Separar pessoas de idade 70+ em fila preferencial");
+		System.out.println("9. Inserir valor X acima das ocorrências do valor 10");
+		System.out.println("10. Passar alunos desclassificados da fila para uma lista");
+		System.out.println("11. Sair do programa");
+	 */
+	
+	// LISTA DE EXERCÍCIOS 6 - QUESTÃO 1 - Retirar valores negativos da fila
+	public void retiraNegativos() {
+		FilaCircular aux = new FilaCircular(this.tamanho);
+		Item atual = null;
+		
+		while (!this.eVazia()) {
+			atual = this.desenfileirar();
+			
+			if (atual.getChave() >= 0) {
+				aux.enfileirar(atual);
+			}
+		}
+		
+		this.enfileirar(aux.desenfileirar());
+	}
+	
+	//  LISTA DE EXERCÍCIOS 6 - QUESTÃO 2 - Somar os valores da fila
+	public int somaValores() {
+		int resultado = 0;
+		
+		while (!this.eVazia()) {
+			resultado += this.desenfileirar().getChave();
+		}
+		
+		return resultado;
+	}
+	
+	//  LISTA DE EXERCÍCIOS 6 - QUESTÃO 3 - Transferir valores de uma pilha para uma fila
+	public FilaCircular pilhaParaFila(PilhaContig pilha) {
+		FilaCircular fila = new FilaCircular(pilha.getTopo() - 1);
+		PilhaContig aux = new PilhaContig(pilha.getTopo());
+		
+		while (!pilha.eVazia()) {
+			aux.empilhar(pilha.desempilhar());
+		}
+		
+		while (!aux.eVazia()) {
+			fila.enfileirar(aux.desempilhar());
+		}
+		
+		return fila;
+	}
+	
+	//  LISTA DE EXERCÍCIOS 6 - QUESTÃO 4 - Imprimir documento em ordem decrescente
+	
 }
--- src/filaCircular/MainFila.java
+++ src/filaCircular/MainFila.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 import dados.Item;
 
 public class MainFila {
+	//  System.out.println("");
 	static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
@@ -14,13 +15,74 @@ public class MainFila {
 		tamanho = scan.nextInt();
 		
 		FilaCircular fila = new FilaCircular(tamanho);
+		for (int a = 0; a < tamanho; a++) {
+			fila.enfileirar(new Item((int) Math.random() * 101));
+		}
 		
 		do {
-			run = Menu();
+			run = Menu(fila);
 		} while (run);
 	}
 	
-	static boolean Menu() {
+	static boolean Menu(FilaCircular fila) {
+		int escolha;
+		
+		System.out.println("Selecione o procedimento a executar:");
+		System.out.println("1. Enfileirar valor");
+		System.out.println("2. Desenfileirar valor");
+		System.out.println("3. Imprimir fila");
+		System.out.println("4. Retirar valores negativos da fila");
+		System.out.println("5. Somar os valores da fila");
+		System.out.println("6. Transferir valores de uma pilha para uma fila");
+		System.out.println("7. Imprimir documento em ordem decrescente");
+		System.out.println("8. Separar pessoas de idade 70+ em fila preferencial");
+		System.out.println("9. Inserir valor X acima das ocorrências do valor 10");
+		System.out.println("10. Passar alunos desclassificados da fila para uma lista");
+		System.out.println("11. Sair do programa");
+		
+		escolha = scan.nextInt();
+		
+		if (escolha == 11) {
+			System.out.println("O programa será encerrado.");
+			return false;
+		}
+		else {
+			switch (escolha) {
+			case 1:
+				
+				return true;
+			case 2:
+						
+				return true;
+			case 3:
+				
+				return true;
+			case 4:
+				
+				return true;
+			case 5:
+				
+				return true;
+			case 6:
+				
+				return true;
+			case 7:
+				
+				return true;
+			case 8:
+				
+				return true;
+			case 9:
+				
+				return true;
+			case 10:
+				
+				return true;
+			default:
+				System.out.println("Opção inválida, o programa será encerrado.");
+				return false;
+			}
+		}
 		
 	}
 }
